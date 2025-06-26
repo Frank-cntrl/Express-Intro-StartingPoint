f32b966139d8be0aafea91e4879c9a5a0a711060
Darrell Moreno <dmoreno1987@outlook.com>
2025-06-26 14:53:21 -0400

    added functionality to get tasks in array and post new tasks

--- api/tasks.js
+++ api/tasks.js
@@ -9,11 +9,25 @@ router.get("/", (req, res) => {
 });
 
 // GET a single task by id
+router.get("/", (req, res) => {
+  const task = Task.findAll();
+  res.json(task);
+});
 
 // Patch a task by id
 
 // Delete a task by id
 
 // Create a new task
+router.post("/", (req, res) => {
+  const { title, description, userId } = req.body;
+  const newTask = Task.create({
+    title,
+    description,
+    completed: false,
+    userId: Number(userId),
+  });
+  res.status(201).json(newTask);
+});
 
 module.exports = router;
